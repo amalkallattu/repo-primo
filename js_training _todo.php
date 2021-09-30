89fc310affd37011c8174a9c99fc05887748a205
Vishnu Das <vishnudas@2hatslogic.com>
2021-09-30 12:45:34 +0530

    new test file added

---
+++ js_training _todo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script> 
+function newElement() { 
+      $("#listTodo").append('<li>'+$("#newTodo").val()+'<button class="close">x</button></li>');
+      $("#newTodo").val("");
+} 
+$(document).on('click','.close', function(){
+   $(this).closest('li').remove();
+}); 
+</script> 
+<title>js training</title>
+</head>
+<body> 
+<div>
+  <h2>JQ To Do List</h2>
+  <input type="text" id="newTodo">
+  <button onclick="newElement()" class="addBtn">Add</button>
+</div> 
+<ul id="listTodo"> 
+</ul> 
+</body> 
+</html>
